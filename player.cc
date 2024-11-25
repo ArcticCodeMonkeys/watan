a36993a7eb983e0e752397ccd223811dc12780a1
Carter Reid <carter@Carters-MacBook-Air.local>
2024-11-25 18:52:24 -0500

    Added very important file

--- player.cc
+++ player.cc
@@ -8,13 +8,13 @@ class Player {
     UnloadedDice Dice; //these should be able to be combined into one i just dont know how.
     public:
     bool completeCriterion(int index) {
-        if(criteria[index]->type == Criterion::TYPE::EMPTY && criteria[index]->player == nullptr && criteria[index]->adjCriteria[0]->player == nullptr && criteria[index]->adjCriteria[1]->player == nullptr && criteria[index]->adjCriteria[2]->player == nullptr) {
+        if(criteria[index]->type == Criterion::Type::EMPTY && criteria[index]->player == nullptr && criteria[index]->adjCriteria[0]->player == nullptr && criteria[index]->adjCriteria[1]->player == nullptr && criteria[index]->adjCriteria[2]->player == nullptr) {
             if(resources["LAB"] > 0 && resources["LECTURE"] > 0 && resources["CAFFEINE"] > 0 && resources["TUTORIAL"] > 0) {
                 resources["LAB"] -= 1;
                 resources["LECTURE"] -= 1;
                 resources["CAFFEINE"] -=1;
                 resources["TUTORIAL"] -= 1;
-                criteria[index]->type = Criterion::TYPE::ASSIGNMENT;
+                criteria[index]->type = Criterion::Type::ASSIGNMENT;
                 return true;
             }
             return false;
@@ -24,22 +24,22 @@ class Player {
         if(criteria[index]->player != this) {
             return false;
         }
-        if(criteria[index]->type == Criterion::TYPE::ASSIGNMENT) {
+        if(criteria[index]->type == Criterion::Type::ASSIGNMENT) {
             if(resources["LECTURE"] > 1 && resources["STUDY"] > 2) {
                 resources["LECTURE"] -= 2;
                 resources["STUDY"] -= 3;
-                criteria[index]->type = Criterion::TYPE::MIDTERM;
+                criteria[index]->type = Criterion::Type::MIDTERM;
                 return true;
             }
             return false;
-        } else if(criteria[index]->type == Criterion::TYPE::MIDTERM) {
+        } else if(criteria[index]->type == Criterion::Type::MIDTERM) {
             if(resources["LAB"] > 1 && resources["LECTURE"] > 1 && resources["CAFFEINE"] > 2 && resources["TUTORIAL"] > 0 && resources["STUDY"] > 1) {
                 resources["LAB"] -= 2;
                 resources["LECTURE"] -= 2;
                 resources["CAFFEINE"] -= 3;
                 resources["TUTORIAL"] -= 1;
                 resources["STUDY"] -= 2;
-                criteria[index]->type = Criterion::TYPE::EXAM;
+                criteria[index]->type = Criterion::Type::EXAM;
                 return true;
             }
             return false;
@@ -54,7 +54,15 @@ class Player {
             return true;
         }
     }
-    void trade(Player p, string resource, std::ostream out) {
-        out << << " offers <colour2> one <resource1> for one <resource2>. Does <colour2> accept this offer?" << endl;
+    void trade(Player p, string ask, string give, std::ostream out, std:istream in) {
+        if(this.resources[ask] == 0)  {
+            
+            return;
+        }
+        if(p.resources[ask])
+        out << this.name << " offers " << p.name << " one " << give << " for one " << ask << ". Does "p.name" accept this offer?" << endl;
+        char confirmation;
+        confirmation << in;
+        if(confirmation)
     }
 };
